adb6dfc07f734bc036390cb3126a8069a2fb3b98
Stefano Maffeis <lesbass@gmail.com>
2021-08-18 23:17:21 +0200

    Update speedtest.sh script with AWS S3 upload

--- speedtest.sh
+++ speedtest.sh
@@ -2,3 +2,5 @@ echo "--------" >> /home/pi/Desktop/speed_log.txt
 date >> /home/pi/Desktop/speed_log.txt
 speedtest --simple >> /home/pi/Desktop/speed_log.txt
 cp /home/pi/Desktop/speed_log.txt /home/pi/myNAS/Public/speed_log.txt
+s3cmd put /home/pi/Desktop/speed_log.txt s3://[bucket]
+s3cmd setacl s3://[bucket]/speed_log.txt --acl-public
